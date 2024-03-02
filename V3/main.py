a660e292202878d09a2699856c7e4f2104a5f01b
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-03-02 23:35:04 +0100

    Update main.py production server

--- V3/main.py
+++ V3/main.py
@@ -71,8 +71,8 @@ def handleIP():
 
 
 if __name__ == '__main__':
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
-    app.run(debug=True, host='0.0.0.0')
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    #app.run(debug=True, host='0.0.0.0')
     
 
